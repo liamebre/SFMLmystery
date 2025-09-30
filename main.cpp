de688b6379412f4a596d47cf47b090e75304cf38
liamebre <164887882+liamebre@users.noreply.github.com>
2025-09-30 14:58:48 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,66 @@
+#include "textBox.h"
+#include "player.h"
+
+int main()
+{
+	float width = 1920;
+	float height = 1080;
+	Vector2u resolution = Vector2u(width,height);
+	Vector2f screenSize = Vector2f(width, height);
+	VideoMode vm(resolution);
+	RenderWindow window(vm, "murderMystery",State::Fullscreen);	
+
+	vector<String> sayings = { "hello","hi","how are you?","goodbye","see you later","bye" };
+	int currSay = 0; // keeps track of the current saying to display 
+
+	Font font;
+	if (!font.openFromFile("fonts/tuffy.ttf")) {cerr << "Error: Could not load font!" << endl;}
+
+	Text& text = *(new Text(font," ",30));
+	textBox tb(screenSize,text);
+
+	player p1({ width / 2,height / 2 }, { 30,30 }, 300);
+
+	Clock clock;
+	Clock gameClock;
+
+	//keeps window open
+	while (window.isOpen()) {
+		Time dt = clock.restart();
+		Time gc = gameClock.getElapsedTime();
+		while (const optional event = window.pollEvent())
+		{
+			if (event->is<Event::Closed>() || Keyboard::isKeyPressed(Keyboard::Key::Escape))
+				// Quit the game when the window is closed
+				window.close();
+		}
+
+		if (Keyboard::isKeyPressed(Keyboard::Key::Enter)) {
+			tb.nextline(gc);
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Key::W)) {
+			p1.moveup();
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Key::S)) {
+			p1.movedown();
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Key::A)) {
+			p1.moveleft();
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Key::D)) {
+			p1.moveright();
+		}
+
+		tb.update(text,sayings,currSay);
+		p1.update(dt);
+
+		// Update the game state
+		window.clear();
+		tb.getTextBox(window,text);
+		window.draw(p1.body);
+		window.display();
+
+	}//end of open window
+
+}//end of main
+
